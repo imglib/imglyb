4b514d2bb79dfa93396fe3cff4d6d5b1767ee1e2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-10 14:40:03 -0400

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from subprocess import call
 from distutils.core import setup
 from distutils.command.build_py import build_py
 
-
-execfile('imglyb/version.py')
+version={}
+with open('imglyb/version.py', 'r') as f:
+    exec(f.read(), version)
 
 setup(
     name='imglyb',
-    version=__version__,
+    version=version['__version__'],
     author='Philipp Hanslovsky',
     author_email='hanslovskyp@janelia.hhmi.org',
     description='A simple python module to bring together the worlds of numpy (Python) and ImgLib2 (Java).',
