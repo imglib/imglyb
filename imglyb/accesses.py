d66d34bcb931c66bdab73ea4fe39bf76d5ad7102
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-16 16:42:10 -0400

    Add volatile support

--- imglyb/accesses.py
+++ imglyb/accesses.py
@@ -4,38 +4,46 @@ import imglyb
 from jnius import autoclass
 
 
-Accesses     = autoclass('tmp.net.imglib2.img.basictypeaccess.Accesses')
+Accesses            = autoclass('tmp.net.imglib2.img.basictypeaccess.Accesses')
 
-ByteArray    = autoclass('net.imglib2.img.basictypeaccess.array.ByteArray')
-CharArray    = autoclass('net.imglib2.img.basictypeaccess.array.CharArray')
-DoubleArray  = autoclass('net.imglib2.img.basictypeaccess.array.DoubleArray')
-FloatArray   = autoclass('net.imglib2.img.basictypeaccess.array.FloatArray')
-IntArray     = autoclass('net.imglib2.img.basictypeaccess.array.IntArray')
-LongArray    = autoclass('net.imglib2.img.basictypeaccess.array.LongArray')
-ShortArray   = autoclass('net.imglib2.img.basictypeaccess.array.ShortArray')
+ByteArray           = autoclass('net.imglib2.img.basictypeaccess.array.ByteArray')
+CharArray           = autoclass('net.imglib2.img.basictypeaccess.array.CharArray')
+DoubleArray         = autoclass('net.imglib2.img.basictypeaccess.array.DoubleArray')
+FloatArray          = autoclass('net.imglib2.img.basictypeaccess.array.FloatArray')
+IntArray            = autoclass('net.imglib2.img.basictypeaccess.array.IntArray')
+LongArray           = autoclass('net.imglib2.img.basictypeaccess.array.LongArray')
+ShortArray          = autoclass('net.imglib2.img.basictypeaccess.array.ShortArray')
 
-ByteUnsafe   = autoclass('net.imglib2.img.basictypelongaccess.unsafe.ByteUnsafe')
-CharUnsafe   = autoclass('net.imglib2.img.basictypelongaccess.unsafe.CharUnsafe')
-DoubleUnsafe = autoclass('net.imglib2.img.basictypelongaccess.unsafe.DoubleUnsafe')
-FloatUnsafe  = autoclass('net.imglib2.img.basictypelongaccess.unsafe.FloatUnsafe')
-IntUnsafe    = autoclass('net.imglib2.img.basictypelongaccess.unsafe.IntUnsafe')
-LongUnsafe   = autoclass('net.imglib2.img.basictypelongaccess.unsafe.LongUnsafe')
-ShortUnsafe  = autoclass('net.imglib2.img.basictypelongaccess.unsafe.ShortUnsafe')
+VolatileByteArray   = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileByteArray')
+VolatileCharArray   = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileCharArray')
+VolatileDoubleArray = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileDoubleArray')
+VolatileFloatArray  = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileFloatArray')
+VolatileIntArray    = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileIntArray')
+VolatileLongArray   = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileLongArray')
+VolatileShortArray  = autoclass('net.imglib2.img.basictypeaccess.volatiles.array.VolatileShortArray')
+
+ByteUnsafe          = autoclass('net.imglib2.img.basictypelongaccess.unsafe.ByteUnsafe')
+CharUnsafe          = autoclass('net.imglib2.img.basictypelongaccess.unsafe.CharUnsafe')
+DoubleUnsafe        = autoclass('net.imglib2.img.basictypelongaccess.unsafe.DoubleUnsafe')
+FloatUnsafe         = autoclass('net.imglib2.img.basictypelongaccess.unsafe.FloatUnsafe')
+IntUnsafe           = autoclass('net.imglib2.img.basictypelongaccess.unsafe.IntUnsafe')
+LongUnsafe          = autoclass('net.imglib2.img.basictypelongaccess.unsafe.LongUnsafe')
+ShortUnsafe         = autoclass('net.imglib2.img.basictypelongaccess.unsafe.ShortUnsafe')
 
 # does not work with strided accesses, currently
-def as_array_access(ndarray):
+def as_array_access(ndarray, volatile=False):
     if ndarray.dtype == np.uint8 or ndarray.dtype == np.int8:
-        return _as_array_access(ndarray, ByteUnsafe, ByteArray)
+        return _as_array_access(ndarray, ByteUnsafe, lambda n : VolatileByteArray(n, True) if volatile else ByteArray(n))
     elif ndarray.dtype == np.uint16 or ndarray.dtype == np.int16:
-        return _as_array_access(ndarray, ShortUnsafe, ShortArray)
+        return _as_array_access(ndarray, ShortUnsafe, lambda n : VolatileShortArray(n, True) if volatile else ShortArray(n))
     elif ndarray.dtype == np.uint32 or ndarray.dtype == np.int32:
-        return _as_array_access(ndarray, IntUnsafe, IntArray)
+        return _as_array_access(ndarray, IntUnsafe, lambda n : VolatileIntArray(n, True) if volatile else IntArray(n))
     elif ndarray.dtype == np.uint64 or ndarray.dtype == np.int64:
-        return _as_array_access(ndarray, LongUnsafe, LongArray)
+        return _as_array_access(ndarray, LongUnsafe, lambda n : VolatileLongArray(n, True) if volatile else LongArray(n))
     elif ndarray.dtype == np.float32:
-        return _as_array_access(ndarray, FloatUnsafe, FloatArray)
+        return _as_array_access(ndarray, FloatUnsafe, lambda n : VolatileFloatArray(n, True) if volatile else FloatArray(n))
     elif ndarray.dtype == np.float64:
-        return _as_array_access(ndarray, DoubleUnsafe, DoubleArray)
+        return _as_array_access(ndarray, DoubleUnsafe, lambda n : VolatileDoubleArray(n, True) if volatile else DoubleArray(n))
 
 def _as_array_access(ndarray, src_clazz, tgt_clazz):
     src = src_clazz(ndarray.ctypes.data)
--- imglyb/types.py
+++ imglyb/types.py
@@ -1,28 +1,55 @@
+import numpy as np
+
 import imglyb
 from jnius import autoclass, MetaJavaClass
 
-NativeType = autoclass( 'net.imglib2.type.NativeType' )
+NativeType = autoclass('net.imglib2.type.NativeType')
 
-FloatType         = autoclass( 'net.imglib2.type.numeric.real.FloatType' )
-DoubleType        = autoclass( 'net.imglib2.type.numeric.real.DoubleType' )
-ByteType          = autoclass( 'net.imglib2.type.numeric.integer.ByteType' )
-UnsignedByteType  = autoclass( 'net.imglib2.type.numeric.integer.UnsignedByteType' )
-ShortType         = autoclass( 'net.imglib2.type.numeric.integer.ShortType' )
-UnsignedShortType = autoclass( 'net.imglib2.type.numeric.integer.UnsignedShortType' )
-IntType           = autoclass( 'net.imglib2.type.numeric.integer.IntType' )
-UnsignedIntType   = autoclass( 'net.imglib2.type.numeric.integer.UnsignedIntType' )
-LongType          = autoclass( 'net.imglib2.type.numeric.integer.LongType' )
-UnsignedLongType  = autoclass( 'net.imglib2.type.numeric.integer.UnsignedLongType' )
+FloatType                 = autoclass('net.imglib2.type.numeric.real.FloatType')
+DoubleType                = autoclass('net.imglib2.type.numeric.real.DoubleType')
+ByteType                  = autoclass('net.imglib2.type.numeric.integer.ByteType')
+UnsignedByteType          = autoclass('net.imglib2.type.numeric.integer.UnsignedByteType')
+ShortType                 = autoclass('net.imglib2.type.numeric.integer.ShortType')
+UnsignedShortType         = autoclass('net.imglib2.type.numeric.integer.UnsignedShortType')
+IntType                   = autoclass('net.imglib2.type.numeric.integer.IntType')
+UnsignedIntType           = autoclass('net.imglib2.type.numeric.integer.UnsignedIntType')
+LongType                  = autoclass('net.imglib2.type.numeric.integer.LongType')
+UnsignedLongType          = autoclass('net.imglib2.type.numeric.integer.UnsignedLongType')
 
+VolatileFloatType         = autoclass('net.imglib2.type.volatiles.VolatileFloatType')
+VolatileDoubleType        = autoclass('net.imglib2.type.volatiles.VolatileDoubleType')
+VolatileByteType          = autoclass('net.imglib2.type.volatiles.VolatileByteType')
+VolatileUnsignedByteType  = autoclass('net.imglib2.type.volatiles.VolatileUnsignedByteType')
+VolatileShortType         = autoclass('net.imglib2.type.volatiles.VolatileShortType')
+VolatileUnsignedShortType = autoclass('net.imglib2.type.volatiles.VolatileUnsignedShortType')
+VolatileIntType           = autoclass('net.imglib2.type.volatiles.VolatileIntType')
+VolatileUnsignedIntType   = autoclass('net.imglib2.type.volatiles.VolatileUnsignedIntType')
+VolatileLongType          = autoclass('net.imglib2.type.volatiles.VolatileLongType')
+VolatileUnsignedLongType  = autoclass('net.imglib2.type.volatiles.VolatileUnsignedLongType')
 
-if __name__ == "__main__":
-	types = {
-		(k, v) for k, v in locals().items() if isinstance( v, ( MetaJavaClass, )  ) and not k in 'NativeType'
-		}
+def for_np_dtype(dtype, volatile=False):
+    if dtype == np.uint8:
+        return VolatileUnsignedByteType() if volatile else UnsignedByteType()
+    if dtype == np.int8:
+        return VolatileByteType() if volatile else ByteType()
 
-	for k, v in types:
-		print( k, v().toString() )
+    if dtype == np.uint16:
+        return VolatileUnsignedShortType() if volatile else UnsignedShortType()
+    if dtype == np.int16:
+        return VolatileShortType() if volatile else ShortType()
 
+    if dtype == np.uint32:
+        return VolatileUnsignedIntType() if volatile else UnsignedIntType()
+    if dtype == np.int32:
+        return VolatileIntType() if volatile else IntType()
 
+    if dtype == np.uint64:
+        return VolatileUnsignedLongType() if volatile else UnsignedLongType()
+    if dtype == np.int64:
+        return VolatileLongType() if volatile else LongType()
 
+    if dtype == np.float32:
+        return VolatileFloatType() if volatile else FloatType()
+    if dtype == np.float64:
+        return VolatileDoubleType() if volatile else DoubleType()
 
