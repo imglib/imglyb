b63b263354c2827d002f7a98155a154fb860076c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-07-03 07:30:10 -0400

    Update version specification

--- imglyb_config.py
+++ imglyb_config.py
@@ -2,7 +2,35 @@ import os
 
 __all__ = ('set_imglib2_imglyb_version', 'get_imglib2_imglyb_version')
 
-version = '0.3.5dev'
+__major__   = 0
+__minor__   = 3
+__patch__   = 5
+__tag__     = 'dev'
+__version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
+
+class _Version(object):
+
+    def major(self):
+        return __major__
+
+    def minor(self):
+        return __minor__
+
+    def patch(self):
+        return __patch__
+
+    def tag(self):
+        return __tag__
+
+    def version(self):
+        return __version__
+
+    def __str__(self):
+        return self.version()
+
+_version = _Version()
+
+version = _version.version()
 
 _default_imglib2_imglyb_version = '0.3.0'
 _imglib2_imglyb_version         = os.getenv('IMGLIB2_IMGLYB_VERSION', _default_imglib2_imglyb_version)
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-version={}
-with open('imglyb/version.py', 'r') as f:
-    exec(f.read(), version)
+version_info={}
+with open('imglyb_config.py', 'r') as f:
+    exec(f.read(), version_info)
+version = version_info['version']
 
 setup(
     python_requires='>=3',
     name='imglyb',
-    version=version['__version__'],
+    version=version,
     author='Philipp Hanslovsky',
     author_email='hanslovskyp@janelia.hhmi.org',
     description='A simple python module to bring together the worlds of numpy (Python) and ImgLib2 (Java).',
