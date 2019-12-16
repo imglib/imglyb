ace3a7fe8eaf9bfcf82dd20b1d3d3124491427b1
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-12-16 08:27:56 -0500

    Test populating cached cell img from array img

---
+++ test_wrap_arraylike_hdf5.py
@@ -0,0 +1,41 @@
+import h5py
+import scyjava_config
+import threading
+import time
+
+scyjava_config.add_endpoints('sc.fiji:bigdataviewer-vistools:1.0.0-beta-18')
+
+import imglyb
+from jnius import autoclass, cast, JavaException
+
+path = '/home/hanslovskyp/Downloads/sample_A_padded_20160501.hdf'
+
+BdvFunctions        = imglyb.util.BdvFunctions
+VolatileTypeMatcher = autoclass('bdv.util.volatiles.VolatileTypeMatcher')
+VolatileViews       = autoclass('bdv.util.volatiles.VolatileViews')
+
+file       = h5py.File(path, 'r')
+ds         = file['volumes/raw']
+block_size = (64, 64, 64)
+img        = imglyb.as_cell_img(ds, block_size, access_type='array', use_volatile_access=True)
+typeClass  = cast('java.lang.Object', img.randomAccess().get())
+print(f'type: {typeClass.getClass().getName()} {VolatileTypeMatcher.getVolatileTypeForType(img.randomAccess().get())}')
+try:
+    vimg       = VolatileViews.wrapAsVolatile(img)
+except JavaException as e:
+    print(e.classname)
+    print(e.innermessage)
+    if e.stacktrace:
+        for s in e.stacktrace:
+            print(s)
+    raise e
+
+bdv = BdvFunctions.show(vimg, 'raw')
+
+def runUntilBdvDoesNotShow():
+    panel = bdv.getBdvHandle().getViewerPanel()
+    while panel.isShowing():
+        time.sleep(0.3)
+
+
+threading.Thread(target=runUntilBdvDoesNotShow).start()
