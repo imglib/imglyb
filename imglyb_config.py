a7c1cf10b8b52b46daebf7ed4313b932cce1ea1a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2020-01-06 16:29:52 -0500

    Release imglyb-0.4.0

--- imglyb_config.py
+++ imglyb_config.py
@@ -3,9 +3,9 @@ import os
 __all__ = ('set_imglib2_imglyb_version', 'get_imglib2_imglyb_version')
 
 __major__   = 0
-__minor__   = 3
-__patch__   = 6
-__tag__     = 'dev0'
+__minor__   = 4
+__patch__   = 0
+__tag__     = ''
 __version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
 
 class _Version(object):
