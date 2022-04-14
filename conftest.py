fe96518861785a1fc30b0b761b6ebc5ec087ee53
Curtis Rueden <ctrueden@wisc.edu>
2022-04-14 15:32:07 -0500

    Run the tests headless
    Otherwise, they hang on macOS.

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ def sj_fixture(request):
     Start the JVM through scyjava once for the whole test environment
     :param request: Pytest variable passed in to fixtures
     """
+    scyjava.config.add_option('-Djava.awt.headless=true')
     scyjava.start_jvm()
 
     yield scyjava
