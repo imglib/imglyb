81e44912fbc11f5310fc60631316d84557eb1ffc
Gabriel Selzer <gjselzer@wisc.edu>
2022-04-18 14:17:55 -0500

    Use setuptools_scm for versioning in sdist

--- src/imglyb/config.py
+++ src/imglyb/config.py
@@ -1,35 +1,7 @@
 import os
+from setuptools_scm import get_version
 
-__major__ = 2
-__minor__ = 0
-__patch__ = 2
-__tag__ = "dev0"
-__version__ = f"{__major__}.{__minor__}.{__patch__}.{__tag__}".strip(".")
-
-
-class _Version(object):
-    def major(self):
-        return __major__
-
-    def minor(self):
-        return __minor__
-
-    def patch(self):
-        return __patch__
-
-    def tag(self):
-        return __tag__
-
-    def version(self):
-        return __version__
-
-    def __str__(self):
-        return self.version()
-
-
-_version = _Version()
-
-version = _version.version()
+version = get_version(root="../..", relative_to=__file__)
 
 _default_imglib2_imglyb_version = "1.0.1"
 _imglib2_imglyb_version = os.getenv(
