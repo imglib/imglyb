7198349729ae33b9e1113c191dddd3fbc03c7ff7
Gabriel Selzer <gjselzer@wisc.edu>
2022-04-19 10:34:59 -0500

    Improve how we obtain versions
    Now imglyb.version.version AND imglyb.__version__ work. Just like Numpy!

--- src/imglyb/__init__.py
+++ src/imglyb/__init__.py
@@ -1,8 +1,10 @@
+from functools import lru_cache
 import logging
 import sys
 
 import scyjava
 import scyjava.config as sjconf
+import imglyb.version
 
 from .config import get_imglib2_imglyb_version
 from .imglib_ndarray import ImgLibReferenceGuard as _ImgLibReferenceGuard
@@ -26,3 +28,30 @@ sjconf.endpoints.append(_IMGLIB2_IMGLYB_ENDPOINT)
 def to_numpy(source):
     scyjava.start_jvm()
     return _ImgLibReferenceGuard(source)
+
+
+def module_property(func):
+    """Decorator to turn module functions into properties.
+    Function names must be prefixed with an underscore."""
+    module = sys.modules[func.__module__]
+
+    def base_getattr(name):
+        raise AttributeError(f"module '{module.__name__}' has no attribute '{name}'")
+
+    old_getattr = getattr(module, "__getattr__", base_getattr)
+
+    def new_getattr(name):
+        if f"_{name}" == func.__name__:
+            return func()
+        else:
+            return old_getattr(name)
+
+    module.__getattr__ = new_getattr
+    return func
+
+
+@module_property
+def ___version__():
+    from pkg_resources import get_distribution
+
+    return get_distribution("imglyb").version
